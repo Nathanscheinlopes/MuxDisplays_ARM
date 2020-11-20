349172a3f2aea96ea364dc2fa5548a050f36f0d7
Nathanscheinlopes <73622095+Nathanscheinlopes@users.noreply.github.com>
2020-11-19 22:19:20 -0300

    Requisitos da Atividade
    Avaliação :
    Serão considerados para avaliação deste trabalho:
    • Codificação (uso adequado das )
    • Documentação do código
    • Resultados apresentados
    
    Co-Authored-By: Liamnroht <74746768+Liamnroht@users.noreply.github.com>

---
+++ Código Principal.c
@@ -0,0 +1,8 @@
+/*O painel deve ser composto por, no mínimo, 4 displays de 7 segmentos.
+ Os displays devem utilizar a técnica de multiplexação para reduzir o uso dos pinos de
+GPIO.
+ As mensagens exibidas devem ter, no mínimo 10 caracteres afanuméricos (incluindo
+espaço)
+ Todos os displays devem aparentar o mesmo brilho.
+ Não pode ocorrer flicker (cintilação) na exibição da mensagem
+*/
