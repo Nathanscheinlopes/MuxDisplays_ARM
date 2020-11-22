31095aabbc51a473aac15ea80672f6e9585da084
Nathanscheinlopes <73622095+Nathanscheinlopes@users.noreply.github.com>
2020-11-22 00:15:28 -0300

    Introdução de funções no código
    AlternarCatodo
    PassosDisplay
    LigarDisplay

--- Código Principal.c
+++ Código Principal.c
@@ -23,26 +23,67 @@ int main()
     GPIOC->MODER&=~(GPIO_MODER_MODER0|GPIO_MODER_MODER1|GPIO_MODER_MODER7);//PC0, PC1 E PC7 SAIDAS
 	GPIOC->MODER|=(GPIO_MODER_MODER0_0|GPIO_MODER_MODER1_0|GPIO_MODER_MODER7_0);    
     //CONFIGURANDO TIMERS//
-    TIM10->PSC = 
-    TIM10->ARR = 
+    TIM10->PSC = 1999; 
+    TIM10->ARR = 9999;
     TIM10->CR1 = 0x1;
 	TIM11->PSC = 
     TIM11->ARR = 
     TIM11->CR1 = 0x1;
     //VARIAVEIS//
-    uint16_t catodo[] = {1110,1101,1011,0111};//Isso vai assegurar que apenas um catodo estará em nivel baixo ao mesmo tempo
+    uint16_t catodo[] = {1111110,1111101,1101111,0111111};//Isso vai assegurar que apenas um catodo estará em nivel baixo ao mesmo tempo
     uint16_t msg[] = {num0,num1,num2,num3,num4,num5,num6,num7,num8,num9};//Por enquanto, 0 a 9, para testes
+    uint16_t kat,jj=0,ii=0;
+
+    while
+    {
+        AlternarCatodo(jj);
+        //PassosDisplay(ii);
+        LigarDisplay(ii);  
+    }
 
 
 
+}
+void AlternarCatodo(uint16_t catodo[])
+{
+    if(TIM11->SR & TIM_SR_UIF)
+    {
+        TIM11->SR = ~TIM_SR_UIF;
+        if (++jj == 4)
+            {
+                jj = 0;
+            }
+        GPIOA->ODR = catodo[jj];
+    }
+}
 
+void PassosDisplay()
+{
+    static ii=0;
+    if(TIM10->SR &TIM_SR_UIF)
+    {
+        TIM10->SR = ~TIM_SR_UIF;
+        if(++ii==11)
+        {
+            ii=0;
+        }   
+    }
+}
 
-
-
-
-
-
-
-
-
+void LigarDisplay(uint16_t msg)
+{
+        uint16_t pc7,pb6,pb3,pb4,pa5;        
+        pc7 = msg[ii] & 0b1000000;
+        pc7 = pc7 << 1;//pc7 como pc6
+        pb6 = msg[ii] & 0b0100000;
+        pb6 = pb6 << 1;//pb6 como pb5
+        GPIOC->ODR = ((msg[ii] & (GPIO_ODR_ODR_0 | GPIO_ODR_ODR_1) | pc7));
+        pb3 = msg[ii] & 0b0000100;
+        pb3 = pb3 << 6;//pb8 como pb3
+        pb4 = msg[ii] & 0b0001000;
+        pb4 = pb4 << 6;//pb9 como pb4
+        pa5 = msg[ii] & 0b0010000;
+        pa5 = pa5 << 3;//pa7 como pa5 
+        GPIOB->ODR = pb3 | pb4 | pb6; 
+        GPIOA->ODR |= pa5;
 }
