3cf94250ebefec339337179acb4604a1e9ef5d6d
Nathanscheinlopes <73622095+Nathanscheinlopes@users.noreply.github.com>
2020-11-26 17:13:25 -0300

    Código Final Funcionando
    Não utlizamos todas as funções que foram pensadas anteriormente, apenas a LigarDisplay.

--- Código Principal.c
+++ Código Principal.c
@@ -14,7 +14,6 @@ uint16_t pc7,pb6,pb3,pb4,pa5;
 //int jj=-1;
 
 
-//void AlternarCatodo(uint16_t* catodo);
 //void PassosDisplay();
 void LigarDisplay(uint16_t* msg, uint16_t aa);
 
@@ -34,36 +33,30 @@ int main()
     TIM10->PSC = 999;
     TIM10->ARR = 9999;
     TIM10->CR1 = 0x1;
-	TIM11->PSC = 7;
-    TIM11->ARR = 3999;
+	TIM11->PSC = 16;
+    TIM11->ARR = 4999;
     TIM11->CR1 = 0x1;
     //VARIAVEIS//
     uint16_t catodo[] = {0b1111110,0b1111101,0b1101111,0b0111111};//Isso vai assegurar que apenas um catodo estará em nivel baixo ao mesmo tempo
-    //uint16_t msg[] = {num0,num0,num1,num2,num3,num4,num5,num6,num7,num8,num9};//Por enquanto, 0 a 9, para testes
-    //uint16_t msg[] = {esp,e,h,esp,u,s,esp,g,u,r,i};
-	uint16_t msg[] = {esp,n,o,z,esp,e,esp,o,esp,j,o,a,o,esp};
-    uint16_t kat,ii=0,duni,ddez,dcem,dmil;
-    duni=ddez=dcem=dmil=0;
-    uint16_t pc7,pb6,pb3,pb4,pa5,aa;
-    int jj=-1;
+	uint16_t msg[] = {esp,e,d,n,a,l,d,o,esp,p,e,r,e,i,r,a,esp};//aqui é onde entra a mensagem, cada letra deve ser separada por virgula e o espaço é abreviado para esp, para numeros utilizar "numX" 
+    uint16_t ii=0,duni,ddez,dcem,dmil,aa;//declarando uma variavel de contagem e as de deslocamento 
+    duni=ddez=dcem=dmil=0;//zerando as variaveis para deslocar o simbolo no display para começar vazio 
+    uint16_t pc7,pb6,pb3,pb4,pa5;//variaveis utilizadas na função LigarDisplay 
+    int jj=-1;//variavel para fazer a leitura do vetor do cátodo
     while(1)
     {
-        //AlternarCatodo(catodo);
-        //PassosDisplay(ii);
-        //LigarDisplay(ii,msg);
-
-    	if(TIM11->SR & TIM_SR_UIF)
+     	if(TIM11->SR & TIM_SR_UIF)
     	    {
     	        TIM11->SR = ~TIM_SR_UIF;
-    	        if (++jj == 4)
+    	        if (++jj == 4)//número de displays presente na multiplexação 
     	            {
     	                jj =0;
     	            }
     	        GPIOA->ODR = catodo[jj];
 
-    	        switch(jj)
+    	        switch(jj)//esse switch é responsável por alternar e deslocar para a próxima variável o valor escrito no primeiro display, registrando os passos 
     	        {
-    	        case 0:
+    	        case 0://display mais a esquerda
     	       	aa=duni;
 				LigarDisplay(msg,aa);
     	        break;
@@ -78,32 +71,29 @@ int main()
     	        LigarDisplay(msg,aa);
     	        break;
 
-    	        case 3:
+    	        case 3://display mais a direita
     	        aa=dmil;
     	        LigarDisplay(msg,aa);
     	        break;
     	        }
-
-
     	    }
-            if(TIM10->SR & TIM_SR_UIF)
+            if(TIM10->SR & TIM_SR_UIF)//quando esse timer de 1s estourar, o display anda um passo 
             {
-            TIM10->SR = ~TIM_SR_UIF;
-
-            dmil = dcem;
-            dcem = ddez;
-            ddez = duni;
-            duni = ii;
-            ii++;
-            if(ii==14)
-            {
-            	ii=1;
-            }
+                TIM10->SR = ~TIM_SR_UIF;
+                dmil = dcem;//deslocamento da variavel para ligar o display  
+                dcem = ddez;//
+                ddez = duni;//
+                duni = ii;//atribuição da variavel de contagem ao display mais a direita 
+                ii++;
+                if(ii==18)//tamanho do vetor com a mensagem a ser exibida
+                {
+                 	ii=1;//reset tamanho do vetor 
+                }
             }
     }
 }
 
-void LigarDisplay(uint16_t* msg,uint16_t aa)
+void LigarDisplay(uint16_t* msg,uint16_t aa)//Essa função vai ligar o display, fazendo deslocamento de bits para adequar ao barramento do arduino
 {
         pc7 = msg[aa] & 0b1000000;
         pc7 = pc7 << 1;//pc7 como pc6
@@ -115,7 +105,7 @@ void LigarDisplay(uint16_t* msg,uint16_t aa)
         pb4 = pb4 << 6;//pb9 como pb4
         pa5 = msg[aa] & 0b0010000;
         pa5 = pa5 << 3;//pa7 como pa5
-        GPIOA->ODR |= pa5;
-        GPIOB->ODR = pb3 | pb4 | pb6;
-        GPIOC->ODR = ((msg[aa] & (GPIO_ODR_ODR_0 | GPIO_ODR_ODR_1) | pc7));
+        GPIOA->ODR |= pa5;//liga os segmentos do GPIOA
+        GPIOB->ODR = pb3 | pb4 | pb6;//liga os segmentos do GPIOB
+        GPIOC->ODR = ((msg[aa] & (GPIO_ODR_ODR_0 | GPIO_ODR_ODR_1) | pc7));//liga os segmentos do GPIOC
 }
